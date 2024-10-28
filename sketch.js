2808694fc927ed2471990a9eec73bec52d121cfc
Xandre <allexandre97@gmail.com>
2024-10-28 22:06:56 +0100

    Moved the sliders to the side of the canvas

--- sketch.js
+++ sketch.js
@@ -80,29 +80,36 @@ function resetSim(){
     frame = 0;
 }
 
+let vertical_spacing = 0;
+let text_x_position;
+
 function setup() {
 
+    _restart = createButton("Restart the Simulation");
+    vertical_spacing += _restart.height + 200;
+    _restart.position(WIDTH*1.05, vertical_spacing);
+    _restart.mouseClicked(resetSim);
+
     _desiredSeparation = createSlider(1, 30, 10, 0);
-    _desiredSeparation.position(0, HEIGHT + 10);
+    vertical_spacing += _desiredSeparation.height*3
+    _desiredSeparation.position(WIDTH*1.05, vertical_spacing);
     let txt_sep = createDiv("Node Separation");
     txt_sep.style("color", "white");
     txt_sep.position(_desiredSeparation.x, _desiredSeparation.y+25);
 
     _separationCohesionRation = createSlider(0.5, 1.5, 1.01, 0);
-    _separationCohesionRation.position(WIDTH/2 - _separationCohesionRation.width/2, HEIGHT + 10);
-    let txt_rat = createDiv("Cohesion:Repulsion Ratio");
+    vertical_spacing += _separationCohesionRation.height*3.5
+    _separationCohesionRation.position(WIDTH*1.05, vertical_spacing);
+    let txt_rat = createDiv("Cohesion - Repulsion Ratio");
     txt_rat.style("color", "white");
     txt_rat.position(_separationCohesionRation.x, _separationCohesionRation.y+25);
 
     _maxEdgeLen = createSlider(0.5, 30, 11, 0);
-    _maxEdgeLen.position(WIDTH - _maxEdgeLen.width, HEIGHT + 10);
+    vertical_spacing += _maxEdgeLen.height*3.5
+    _maxEdgeLen.position(WIDTH*1.05, vertical_spacing);
     let txt_len = createDiv("Max Edge Length");
     txt_len.style("color", "white");
     txt_len.position(_maxEdgeLen.x, _maxEdgeLen.y+25);
-
-    _restart = createButton("Restart the Simulation");
-    _restart.position(WIDTH*(1+0.05), _restart.height);
-    _restart.mouseClicked(resetSim);
 
 
     Line = new DifferentialLine(500, _maxForce, _maxSpeed, _desiredSeparation.value(), 
